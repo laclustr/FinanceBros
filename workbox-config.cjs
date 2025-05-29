835e67df6f5d5ded26f0ef26ebcb8742fc89caf7
laclustr <gant.a.vance@gmail.com>
2025-05-29 06:31:38 -0400

    Idk why it refuses to cache

--- workbox-config.cjs
+++ workbox-config.cjs
@@ -1,14 +1,38 @@
 module.exports = {
-	globDirectory: 'public/',
+	globDirectory: 'dist/',
 	globPatterns: [
-	  '**/*.{svg,jpg,js,css,html,json,astro}'
+	  '**/*.{html,js,css,svg,jpg,png,json,woff2}'
 	],
-	swDest: 'public/sw.js',
+	swDest: 'dist/sw.js',
 	ignoreURLParametersMatching: [
 	  /^utm_/,
 	  /^fbclid$/
 	],
+	clientsClaim: true,
 	skipWaiting: true,
-	clientsClaim: true
+	runtimeCaching: [
+	  {
+		urlPattern: ({ request }) => request.mode === 'navigate',
+		handler: 'NetworkFirst',
+		options: {
+		  cacheName: 'pages-cache',
+		  networkTimeoutSeconds: 3,
+		  expiration: { maxEntries: 50 },
+		},
+	  },
+	  {
+		urlPattern: ({ request }) => request.destination === 'style' || request.destination === 'script',
+		handler: 'StaleWhileRevalidate',
+		options: { cacheName: 'assets-cache' },
+	  },
+	  {
+		urlPattern: ({ request }) => request.destination === 'image',
+		handler: 'CacheFirst',
+		options: {
+		  cacheName: 'images-cache',
+		  expiration: { maxEntries: 50, maxAgeSeconds: 30 * 24 * 60 * 60 },
+		},
+	  },
+	],
   };
   
