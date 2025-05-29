73f8113e93b1d54c1ea90cb115d7587100a880f2
laclustr <gant.a.vance@gmail.com>
2025-05-29 07:12:43 -0400

    change

---
+++ workbox-config.prod.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+	globDirectory: 'dist/',
+	globPatterns: [
+	  '**/*.{html,js,css,svg,jpg,png,json,woff2,webmanifest}'
+	],
+	swDest: 'dist/sw.js',
+	ignoreURLParametersMatching: [
+	  /^utm_/,
+	  /^fbclid$/
+	],
+	clientsClaim: true,
+	skipWaiting: true,
+	runtimeCaching: [
+	  {
+		urlPattern: ({request}) => request.mode === 'navigate',
+		handler: 'NetworkFirst',
+		options: {
+		  cacheName: 'pages-cache',
+		  networkTimeoutSeconds: 3,
+		  expiration: { maxEntries: 50 },
+		},
+	  },
+	  {
+		urlPattern: ({request}) => request.destination === 'script' || request.destination === 'style',
+		handler: 'StaleWhileRevalidate',
+		options: { cacheName: 'assets-cache' },
+	  },
+	  {
+		urlPattern: ({request}) => request.destination === 'image',
+		handler: 'CacheFirst',
+		options: {
+		  cacheName: 'images-cache',
+		  expiration: { maxEntries: 50, maxAgeSeconds: 30 * 24 * 60 * 60 },
+		},
+	  },
+	],
+  };
+  
