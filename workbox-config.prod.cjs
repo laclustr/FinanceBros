8498485ab4b45d6cd04ea3e6666f66621d6bcec9
laclustr <gant.a.vance@gmail.com>
2025-06-01 11:32:51 -0400

    tst

--- workbox-config.prod.cjs
+++ workbox-config.prod.cjs
@@ -3,7 +3,7 @@ module.exports = {
   globPatterns: [
     '**/*.*'
   ],
-  swDest: '/sw.js',
+  swDest: 'dist/sw.js',
   swSrc: 'public/sw-base.js',
   maximumFileSizeToCacheInBytes: 100 * 1024 * 1024
 };
