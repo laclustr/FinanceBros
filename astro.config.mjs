a005e33c3b8128d21be7f3a66ab8dfd73153a7f3
laclustr <gant.a.vance@gmail.com>
2025-06-01 19:03:32 -0400

    tst

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
-import AstroPWA from '@vite-pwa/astro';
 
 export default defineConfig({
   vite: {
@@ -9,26 +8,4 @@ export default defineConfig({
   },
   output: 'server',
   adapter: vercel(),
-  integrations: [
-    AstroPWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Finance Bros',
-        short_name: 'Finance Bros',
-        description: 'Finance Bros App',
-        theme_color: '#ff00ff',
-        background_color: '#ff22ff',
-        display: 'standalone',
-        start_url: '/dashboard',
-        icons: [
-          { src: '/icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' },
-        ],
-      },
-      devOptions: {
-        enabled: true,
-        type: 'module',
-      },
-    }),
-  ],
 });
