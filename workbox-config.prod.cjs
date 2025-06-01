1244f5a8d32a4607a8b1b42655669707b30823de
laclustr <gant.a.vance@gmail.com>
2025-06-01 11:42:35 -0400

    sas

--- workbox-config.prod.cjs
+++ workbox-config.prod.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   globDirectory: 'dist/',
   globPatterns: [
-    '**/*.*'
+    '**/*.{html,js,css,png,jpg,jpeg,gif,svg,webp,avif,ico,woff,woff2,ttf,eot,json,xml,txt,pdf}'
   ],
   swDest: 'dist/sw.js',
   swSrc: 'public/sw-base.js',
