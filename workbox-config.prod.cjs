91bd90d25d831948edeb7eae155dac2cca1ff3b2
laclustr <gant.a.vance@gmail.com>
2025-06-01 13:54:27 -0400

    asd

--- workbox-config.prod.cjs
+++ workbox-config.prod.cjs
@@ -1,9 +1,9 @@
+const path = require('path');
+
 module.exports = {
-  globDirectory: 'dist/',
-  globPatterns: [
-    '**/*.*'
-  ],
-  swDest: 'public/sw.js',
-  swSrc: 'public/base.js',
+  globDirectory: path.resolve(__dirname, 'dist/client'),
+  globPatterns: ['**/*.*'],
+  swDest: path.resolve(__dirname, 'dist/client/sw.js'),
+  swSrc: path.resolve(__dirname, 'public/base.js'),
   maximumFileSizeToCacheInBytes: 100 * 1024 * 1024
 };
